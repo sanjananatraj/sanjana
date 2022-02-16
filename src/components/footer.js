5bee2880ec253984ebd6b082260e4bfa5c3ba132
Sanjana Natraj <sanjana.natraj319@gmail.com>
2022-02-16 14:48:10 -0800

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,43 @@
 import * as React from "react"
 import { footer } from "./footer.module.scss"
+import github from "../images/icons8-github-100.png"
+import resume from "../images/icons8-resume-100.png"
+import linkedin from "../images/icons8-linkedin-100.png"
+
+const links = [
+  { url: 'https://github.com/sanjananatraj', label: 'Github', icon: github },
+  { url: 'https://www.linkedin.com/in/sanjana-natraj/', label: 'LinkedIn', icon: linkedin },
+  { url: '/', label: 'Resume', icon: resume }
+]
 
 const Footer = () =>{
-    return(
-        <footer className={footer}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby.</a>
-          {` `}
-          <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/Tfx7uIa5vMTc/moon">Moon</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
-        </footer>
-    )
+  return(
+    <footer className={footer}>
+      © {new Date().getFullYear()} Sanjana Natraj. Built with
+      {` `}
+      <a target="_blank" rel="noreferrer" href="https://www.gatsbyjs.com">Gatsby</a>.
+      {` `}
+      Icons by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+      <br></br>
+      <br></br>
+
+      <section>
+        <nav>
+          {links.map((link) => (
+            <a 
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+              key={link.label}
+            >
+              <img src={link.icon} alt={link.label} />
+              <span title={link.label}>{link.label}</span>
+            </a>
+          ))}
+        </nav>
+      </section>
+    </footer>
+  )
 }
 
 export default Footer
