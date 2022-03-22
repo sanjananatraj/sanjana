1184820b395f400b9b4af6175ec1973181ac58fb
Sanjana Natraj <sanjana.natraj319@gmail.com>
2022-03-22 14:55:52 -0700

    all project frontmatter added

--- src/components/sections/projectsSection.js
+++ src/components/sections/projectsSection.js
@@ -6,17 +6,15 @@ import * as styles from "../../styles/sections.module.scss"
 import sun from "../../images/icons8-sun-128.png"
 
 const ProjectSection = ({ pageRefs }) => {
-
   const data = useStaticQuery(graphql`
-    query  {
+    query {
       allMdx(sort: {fields: frontmatter___order, order: ASC}) {
         nodes {
           frontmatter {
-            date
             external
             github
             summary
-            technologies
+            stack
             title
             order
             image {
@@ -24,6 +22,7 @@ const ProjectSection = ({ pageRefs }) => {
                 gatsbyImageData
               }
             }
+            image_alt
           }
         }
       }
@@ -39,13 +38,11 @@ const ProjectSection = ({ pageRefs }) => {
         {data.allMdx.nodes.map((node) => (
           <div className={styles.projectItem} key={node.order}>
               <h6 className={styles.title}>
-                <GatsbyImage
-                  image={getImage(node.frontmatter.image)}
-                />
+                <GatsbyImage image={getImage(node.frontmatter.image)} alt={node.frontmatter.image_alt} />
                 {node.frontmatter.title}
               </h6>
               
-              <p className={styles.summary}>{node.frontmatter.summary}</p>
+              <p>{node.frontmatter.summary}</p>
 
               <nav className={styles.links}>
                 {node.frontmatter.github && (
@@ -56,51 +53,14 @@ const ProjectSection = ({ pageRefs }) => {
                   <a target="_blank" rel="noreferrer" href={node.frontmatter.external}>Link</a>
                 )}
               </nav>
-            
+          
               <div className={styles.techList}>
-                {node.frontmatter.technologies.map((tech, index) => (
+                {node.frontmatter.stack.map((tech, index) => (
                   <span key={index}>{tech}</span>
                 ))}
               </div>
-
           </div>
         ))}
-      
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>Front End Developer Program</h6>
-          <p>Praesent ac neque a erat laoreet dignissim at eu ipsum. Integer vitae neque non est consequat malesuada. Morbi consequat metus ut dui bibendum, a feugiat lorem luctus.</p>
-        </div>
-
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>Placeholder Title</h6>
-          <p>Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla. Pharetra vel turpis nunc eget lorem dolor sed viverra. Venenatis urna cursus eget nunc.</p>
-        </div>
-
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>🥰</h6>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>Lorem Ipsum...</h6>
-          <p>...sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      </div>
-
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>Title 🤗</h6>
-          <p>Just a couple of words for this one</p>
-        </div>
-
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>Just a placeholder text. Here's a longer title</h6>
-          <p>TODO: Now, I have to generate my list of projects programmatically!</p>
-          <p>Initial thoughts: How can I leverage GraphQL to expose my data? 🤔</p>
-        </div>
-
-        <div class={styles.projectItem}>
-          <h6 class={styles.title}>A Project</h6>
-          <p>This is a cool app that does cool things.</p>
-        </div>
       </div>
     </section>
   )
