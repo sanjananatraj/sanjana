8a97a9ccb5be15f248c6f199050a44b39174f4d2
Sanjana Natraj <sanjana.natraj319@gmail.com>
2022-03-24 21:07:06 -0700

    base about info added

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,6 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -32,6 +31,23 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/icons8-new-moon-64.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/sections/aboutme.js
+++ src/components/sections/aboutme.js
@@ -14,9 +14,9 @@ const AboutMe = ({ pageRefs }) => {
   
   return(
       <section id="aboutme" ref={el => pageRefs.current = { ...pageRefs.current, aboutme: el}}>
-        <h1><img src={night} alt="cloud and a moon"></img>{' '}About me</h1>
+        <h1><img src={night} alt="cloud and a moon"></img>{' '}About</h1>
         
-        <p>Hello! My name is Sanjana, and I 💗 building beautiful websites, interfaces, and apps. I particularly love all things web development and front-facing software. Based in the Bay Area, my interest in coding started when I was in high school, as I was surrounded by all sorts of people in tech. I particularly liked creating and designing websites with HTML, CSS and JavaScript. I then joined my local Girls Who Code club, which accelerated my interest in coding.</p>
+        <p>Hello! My name is Sanjana, and I 💗 building beautiful websites, interfaces, and apps. I particularly love all things web development and front-facing software. Based in the Bay Area, my interest in coding started when I was in high school, as I was surrounded by all sorts of people in tech. I particularly liked creating and designing websites with HTML, CSS and JavaScript.</p>
 
         <p>While my main interests lie in front end development, I also have a bit of experience in backend technology, such as utilizing Flask and MySQL to build <a target="_blank" rel="noreferrer" href="https://thirdwavecoffeebase.com/">my own RESTful API</a>. Some of my favorite courses in college were about databases, machine learning, NLP, and data visualization.</p>
         
