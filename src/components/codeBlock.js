1769fcf07129c965371fc31bd95d3f0b14476f28
Sanjana Natraj <sanjana.natraj319@gmail.com>
2022-04-07 19:11:01 -0700

    changed theme a bit

--- src/components/codeBlock.js
+++ src/components/codeBlock.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Highlight, { defaultProps } from "prism-react-renderer"
-import theme from "prism-react-renderer/themes/nightOwl"
+import theme from "../themes"
 
 const CodeBlock = (props) => {
   const className = props.children.props.className || ""
